4e1367584212ecf0cb65283b79e6d10244f2c542
atticweb <zolderweb@gmail.com>
2016-08-01 14:38:26 +0200

    added functional test for the api

--- tests/AppBundle/Controller/GameDataControllerTest.php
+++ tests/AppBundle/Controller/GameDataControllerTest.php
@@ -3,7 +3,17 @@
 namespace tests\AppBundle\Controller;
 
 
-class GameDataControllerTest
-{
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+class GameDataControllerTest extends WebTestCase
+{
+    public function testGetMapData()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/getMapData', array('lat' => '52.3702160', 'lng' => '4.8951680'), array(), array('HTTP_X-Requested-With' => 'XMLHttpRequest'));
+        $response = $client->getResponse();
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('application/json', $response->headers->get('Content-Type'));
+        $this->assertNotEmpty($response->getContent());
+    }
 }
